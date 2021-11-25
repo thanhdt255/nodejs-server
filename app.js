941f43df95a97ed663f7f174c0126a5a6b49e427
thanhdt255 <92931908+thanhdt255@users.noreply.github.com>
2021-11-25 21:37:36 +0700

    update

--- app.js
+++ app.js
@@ -8,18 +8,8 @@ router.get('/', function(req, res) {
     //__dirname : It will resolve to your project folder.
 });
 
-router.get('/about', function(req, res) {
-    res.sendFile(path.join(__dirname + '/about.html'));
-});
-
-router.get('/sitemap', function(req, res) {
-    res.sendFile(path.join(__dirname + '/sitemap.html'));
-});
-
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
 
-
-app.use(express.static(__dirname + 'anhfpt'));
 console.log('Running at Port 3000');
