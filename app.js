efc81e415be97319946f15321641ac18f9afd181
thanhdt255 <92931908+thanhdt255@users.noreply.github.com>
2021-11-25 21:16:45 +0700

    update

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
-router.get('/about',function(req,res){
-    res.sendFile(path.join(__dirname+'/about.html'));
+router.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
 });
 
-router.get('/sitemap',function(req,res){
-    res.sendFile(path.join(__dirname+'/sitemap.html'));
+router.get('/sitemap', function(req, res) {
+    res.sendFile(path.join(__dirname + '/sitemap.html'));
 });
 
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
 
-app.use(express.static(__dirname+'anhfpt'));
+
+app.use(express.static(__dirname + 'anhfpt'));
 console.log('Running at Port 3000');
